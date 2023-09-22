732857e1d516c5d77f6831ca3055455c206f39ce
MillerMaker <cohen11miller@gmail.com>
2023-09-22 17:34:44 -0400

    Removed ListGroup

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Message from '../Message'
 import { Auth } from '../components/auth'
-import ListGroup from '../components/ListGroup'
 
 function HomePage() {
   return (
     <div>
         <Message />
         <Auth />
-        <ListGroup />
     </div>
   )
 }
