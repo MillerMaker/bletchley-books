a0c2221781eeb31c19f69f3839d7a8662afd62a0
LeafesMan <pyromaniac564332@gmail.com>
2023-09-22 21:01:02 -0400

    Basic User List functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import './App.css'
+import UserList from "./components/UserList"
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
 import CreateNewUser from './pages/CreateNewUser'
 import HomePage from './pages/HomePage'
+import AdminPage from './pages/AdminPage'
+
 
 
 function App() {
@@ -11,7 +14,8 @@ function App() {
       <main>
         <Routes>
           <Route path='' element={<HomePage/>}/>
-          <Route path='newuser' element={<CreateNewUser/>}/>
+                  <Route path='newuser' element={<CreateNewUser />} />
+                  <Route path='adminpage' element={<AdminPage />} />
         </Routes>
       </main>
     </BrowserRouter>
