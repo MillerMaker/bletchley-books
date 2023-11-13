4760deae2eec38825ecec2366283993c17986b5b
MillerMaker <cohen11miller@gmail.com>
2023-11-13 13:58:09 -0500

    Added Financial Statements Popup

--- src/components/ChartAccounts.tsx
+++ src/components/ChartAccounts.tsx
@@ -4,13 +4,15 @@ import {saveDocAt, db, GetAuthUserDoc, getDocAt, getErrorMessage } from '../fire
 import NewAccountPopup from './NewAccountPopup';
 import Alert from './Alert';
 import { useNavigate} from "react-router-dom";
-import calendarImage from "../assets/calendar-icon.png"
-import "./Header.css"
+import calendarImage from "../assets/calendar-icon.png";
+import reportsImage from "../assets/file-icon.svg";
+import "./Header.css";
 import CustomPopup from './CustomPopup';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import EmailUserList from './EmailUserList';
 import './ChartAccounts.css'
+import FinancialStatements from './FinancialStatements';
 
 
 
@@ -26,6 +28,7 @@ function ChartAccounts() {
     const [editPopupShown, setEditPopupShown] = useState(false);
     const [calendarPopupShown, setCalendarPopupShown] = useState(false);
     const [emailPopupShown, setEmailPopupShown] = useState(false);
+    const [statementsPopupShown, setStatementsPopupShown] = useState(false);
 
     //Alert State
     const [alertShown, setAlertShown] = useState(false);
@@ -113,8 +116,26 @@ function ChartAccounts() {
         <>
             {alertShown && <Alert text={alertText} color={alertColor}></Alert>}
             <div>
-                <button className='calendar-button' onClick={() => {setCalendarPopupShown(true)}}><img src={calendarImage} className='calendar'/></button>
-                <label>Search:</label>
+            <div className ='functions'>
+                <button className='calendar-button' onClick={() => {setCalendarPopupShown(true)}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="41" height="41" fill="currentColor" className="bi bi-calendar-event-fill file" viewBox="0 0 16 16">
+                    <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z"/>
+                    <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
+                    </svg>  
+                </button>
+
+                <svg onClick={() => setEmailPopupShown(true)} xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" className="bi bi-envelope" viewBox="0 0 16 16">
+                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+                </svg>
+                <div className='reports-button'   title="View Financial Statements" onClick={() => setStatementsPopupShown(true)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill= "currentColor" className="bi bi-file-earmark-bar-graph-fill file" viewBox="0 0 16 16"> 
+                    <path d="M4 11a1 1 0 1 1 2 0v1a1 1 0 1 1-2 0v-1zm6-4a1 1 0 1 1 2 0v5a1 1 0 1 1-2 0V7zM7 9a1 1 0 0 1 2 0v3a1 1 0 1 1-2 0V9z"/>
+                    <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+                    <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+                    </svg>
+                </div>
+            </div>
+                            <label>Search:</label>
                 <select
                     value={searchColumn}
                     onChange={(e) => { setSearchColumn(e.target.value) }}
@@ -132,21 +153,7 @@ function ChartAccounts() {
                     value={searchText}
                     onChange={(e) => { setSearchText(e.target.value) }}
                 />
-                {isAdmin && //Only Show Create Account if user is Admin
-                    <button title="Create a new Account"
-                        className="btn-block btn btn-success long" onClick={() => setCreatePopupShown(true)}
-                    >
-                        Create Account
-                    </button>
-                }
-                <button title="Email User"
-                        className="btn btn-primary" onClick={() => setEmailPopupShown(true)}
-                    >
-                        Email
-                    </button>
             </div>
-            
-            <br></br><br></br>
             {accountDocs.length == 0 && <p>No Accounts Found</p>}
             <table className="table table-bordered table-hover">
                 <thead>
@@ -211,6 +218,14 @@ function ChartAccounts() {
                 </div>
             }
 
+            {isAdmin && //Only Show Create Account if user is Admin
+            <button title="Create a new Account"
+                className="btn-block btn btn-success btn-sm long" onClick={() => setCreatePopupShown(true)}
+            >
+                Create Account
+            </button>
+            }   
+
             {createPopupShown && //Show Change Role Popup if Change Role Popup Shown
                 <NewAccountPopup backCallback={() => setCreatePopupShown(false)} confirmCallback={() => { setRequestedData(false); }} toEdit={{ id: "null", data: null }} />
             }
@@ -235,6 +250,16 @@ function ChartAccounts() {
                     </>
             } />
             } 
+            {statementsPopupShown && 
+                <CustomPopup child={
+                    <>
+                        <FinancialStatements></FinancialStatements>
+                        <br></br>
+                        <button className="btn btn-primary btn-secondary" onClick={() => {setStatementsPopupShown(false)}}>Close</button>
+                    </>
+            } />
+            } 
+
         </>
     );
 }
