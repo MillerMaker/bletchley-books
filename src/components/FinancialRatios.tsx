d3e59febb99e0bd3258b56067f50ca3f73191419
MillerMaker <cohen11miller@gmail.com>
2023-11-27 18:35:29 -0500

    Added-Color-Coding

--- src/components/FinancialRatios.tsx
+++ src/components/FinancialRatios.tsx
@@ -10,9 +10,9 @@ function FinancialRatios() {
     const [requestedData, setRequestedData] = useState(false);
 
     /*ACCOUNT TOTALS BY CATEGORY*/
-    const [totalAssets, setTotalAssets] = useState(0)
-    const [totalLiabilities, setTotalLiabilities] = useState(0)
-    const [totalSales, setTotalSales] = useState(0)
+    const [totalEquity, setTotalEquity] = useState(0);
+    const [totalLiability, setTotalLiability] = useState(0);
+    const [totalAsset, setTotalAsset] = useState(0);
 
     /* RATIO VALUES */
     const [currentRatio, setCurrentRatio] = useState(0);
@@ -33,53 +33,48 @@ function FinancialRatios() {
         /* REQUEST DATA ONCE */
         setRequestedData(true);
 
-        /* GET ACCOUNTS */
-        let q = query(collection(db, "accounts"));
-        let accountQuery = await getDocs(q)
+        let allAccounts = query(collection(db, "accounts"));
+        let queryResult = await getDocs(allAccounts);
 
-        let accountDocs: Array<{ id: string, data: any }> = new Array();
-        accountQuery.forEach((doc) => {
-            let d = doc.data(); 
-            if (d.normalSide == "debit") { d.debit = d.initialBalance; d.credit =0} else {d.credit = d.initialBalance; d.debit = 0;}  
-            accountDocs.push({ id : doc.id,  data : d });
-        })
+        let equityAcc: Array<{ id: string, data: any }> = new Array();
+        let liabilityAcc: Array<{ id: string, data: any }> = new Array();
+        let assetAcc: Array<{ id: string, data: any }> = new Array();
 
         /* GET JOURNAL ENTRIES */
         let journalDocs = query(collection(db, "journals"));
         let journalQuery = await getDocs(journalDocs);
 
         let assets = 0;
+        let liabilities = 0;
+        let equity =  0;
 
-        journalQuery.forEach((journalDoc) => {
-            //console.log(state.data.startDate);
-            const transactions = journalDoc.data().transactions;
-            //Check journal entry approved
-            if(journalDoc.data().status == "approved") {
-                //for each transaction, get the associated account, and add the credits and debits
-                transactions.map(((trans: { id: string, credit: number, debit: number }) => { 
-                    let i = accountDocs.findIndex((infoObj: {id: string, data: any}) => infoObj.id == trans.id);
-                    console.log(accountDocs[i].data.category)
-                    //Tally Total Assets, Liabilities, Sales, Debt, Equity
-                    switch(accountDocs[i].data.category) {
-                        case "asset":
-                                console.log("Total Assets" + assets)
-                                assets = assets + trans.debit - trans.credit
-                                break;
-                    }
-                    accountDocs[i].data.credit = accountDocs[i].data.credit + trans.credit;
-                    accountDocs[i].data.debit = accountDocs[i].data.debit + trans.debit;
-                }));
+        queryResult.forEach((doc) => {
+            if (doc.data().category == 'asset') {
+                assetAcc.push({ id: doc.id, data: doc.data() });
+                assets = assets + ( doc.data().initialBalance - doc.data().credit + doc.data().debit);
+            } else if (doc.data().category == 'liability') {
+                liabilityAcc.push({ id: doc.id, data: doc.data() });
+                liabilities = liabilities + ( doc.data().initialBalance + doc.data().credit - doc.data().debit);
+            }else if (doc.data().category == 'equity') {
+                equityAcc.push({ id: doc.id, data: doc.data() });
+                equity = equity + (doc.data().initialBalance + doc.data().credit - doc.data().debit);
+            } else {
+                console.log("Something went wrong getting the financial statements");
             }
         })
 
-        calculateRatios(assets)
+        setTotalAsset(assets)
+        setTotalLiability(liabilities)
+        setTotalEquity(equity)
+
+        calculateRatios(assets, liabilities, equity)
         setAccountDocs(accountDocs);
 
     }
 
-    function calculateRatios(assets: number) {
+    function calculateRatios(assets: number, liabilities: number , equity : number) {
         /*Current Ratio = Current Assets/Current Liabilities */
-        setCurrentRatio(assets)
+        setCurrentRatio(assets/liabilities)
         /*Cash Ratio = (Cash + Marketable Securities)/(Current Liabilities) */
         /*Receivables Turnover = (Annual Credit Sales)/(Accounts Recivable) */
         /*Inventory Turnover = (Cost of Goods Sold)/(Average Inventory) */
@@ -108,7 +103,7 @@ function FinancialRatios() {
                                 </tr>
                                 <tr>
                                     <td className ="name">Current Ratio</td>
-                                    <td className = "debits" >{currentRatio.toLocaleString()}</td>
+                                    <td className = {currentRatio > 2 ? 'warning' : currentRatio < 1? 'danger' : "normal" }>{currentRatio.toLocaleString()}</td>
                                 </tr>
                                 <tr>
                                     <td className ="name">Cash Ratio</td>
